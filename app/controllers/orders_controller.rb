976eb2aa20cdcfe831f03972d6ed626f69721f96
kevinhylant <kevin.hylant@gmail.com>
2014-11-05 16:15:14 -0500

    small changes with erb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,17 @@
 class OrdersController < ApplicationController
 
-
   def new   #leftover_entry_page
     @order = Order.new
-    @order.recipes.build
   end
   
   def create
-    
-    @order = Order.new(params[:leftovers[])
-
-    if @list.save
-      redirect_to @list
-      # redirect_to list_path(@list)
-    else
-      render :new
-    end
+    @order = Order.new(params[:leftovers)
+    redirect "orders/#{@order.id}"
   end
 
-  # def show
-  #   @list = List.find(params[:id])
-  # end
+  def show
+    @order = Order.find(params[:id])
+  end
 
   # def index
   #   @lists = List.all
